aba89b2a0c2ec71be2061e3f27fcac8eac2f6191
BarbaraGB <barbaragimeno.cm@gmail.com>
2023-09-20 19:25:21 +0200

    Array

---
+++ 202309-w2ch3-barbara-gimeno.js
@@ -0,0 +1,50 @@
+const songs = [
+  {
+    id: 1,
+    title: "We are the champion",
+    artist: "Queen",
+    genre: "Rock",
+    duration: 280,
+    year: 1977,
+    wasHit: true,
+  },
+  {
+    id: 2,
+    title: "Macarena",
+    artist: "Los del rio",
+    genre: "Pop",
+    duration: 198,
+    year: 1962,
+    wasHit: true,
+  },
+  {
+    id: 3,
+    title: "Soy peor",
+    artist: "Soy peor",
+    genre: "Reggeaton",
+    duration: 220,
+    year: 2016,
+    wasHit: true,
+  },
+  {
+    id: 4,
+    title: "Sensaciones",
+    artist: "Sen Senra",
+    genre: "Pop",
+    duration: 240,
+    year: 2019,
+    wasHit: false,
+  },
+ const newSong
+ {
+    id: 4,
+    title: "Sensaciones",
+    artist: "Sen Senra",
+    genre: "Pop",
+    duration: 240,
+    year: 2019,
+    wasHit: false,
+  },
+
+
+
